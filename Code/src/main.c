ecd72af08673d04bb1a2ad30c83382954998c352
NorthTheval <norththeval@protonmail.com>
2025-11-03 10:40:26 +0100

    Etape 1

--- Code/src/main.c
+++ Code/src/main.c
@@ -17,6 +17,11 @@ int main()
         // Draw
         DrawTexture(background, 0, 0, WHITE);
 
+        //Text
+        DrawText("(F) : Launch Solo Game", 160, 350, 20, WHITE);
+        DrawText("(K) : Launch Duo Game", 160, 400, 20, WHITE);
+        DrawText("(Esc) : Quit Game", 160, 450, 20, WHITE);
+
         EndDrawing();
     }
 
