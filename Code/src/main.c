bb6d7f93fa6e7f3b140a427eefefedd5c1e1f65b
NorthTheval <norththeval@protonmail.com>
2025-11-03 09:41:02 +0100

    Project Base

---
+++ Code/src/main.c
@@ -0,0 +1,28 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(640, 800, "MineStorm");
+    SetTargetFPS(60);
+
+    Texture2D background = LoadTexture("Assets/minestorm_background.png");
+
+    while (!WindowShouldClose())
+    {
+        // Update
+        
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        // Draw
+        DrawTexture(background, 0, 0, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+
+    CloseWindow();
+
+    return 0;
+}
