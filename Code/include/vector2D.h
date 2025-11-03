81ec84b3deac9184bf3dd44c540559c929c5e33a
NorthTheval <norththeval@protonmail.com>
2025-11-03 10:07:43 +0100

    Update headers

--- Code/include/vector2D.h
+++ Code/include/vector2D.h
@@ -1,7 +1,7 @@
 typedef struct Vector2D
 {
-float x;
-float y;
+	float x;
+	float y;
 } Vector2D;
 
 Vector2D Vector2D_SetFromComponents(float x, float y);
@@ -15,8 +15,8 @@ float Vector2D_Norm(Vector2D v);
 float Vector2D_DotProduct(Vector2D v1, Vector2D v2);
 float Vector2D_CrossProduct(Vector2D v1, Vector2D v2);
 float Vector2D_GetAngle(Vector2D v1, Vector2D v2);
+
 Vector2D Vector2D_Translate(Vector2D p, Vector2D t); // same as Add function
 Vector2D Vector2D_Scale(Vector2D p, float a, Vector2D anchor);
 Vector2D Vector2D_Rotate(Vector2D p, float theta, Vector2D anchor);
-
 void Vector2D_Print(Vector2D v);
