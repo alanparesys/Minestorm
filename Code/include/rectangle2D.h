eaeb3f432987f9c75823d253ac5d8225754548a1
Louis-Gil-Silveira <gilsilveiralouis@gmail.com>
2025-11-07 10:09:21 +0100

    best changements ever

--- Code/include/rectangle2D.h
+++ Code/include/rectangle2D.h
@@ -23,6 +23,7 @@ float Rectangle2D_GetSurface(Rectangle2D rect);
 Rectangle2D Rectangle2D_Translate(Rectangle2D rect, Vector2D t);
 Rectangle2D Rectangle2D_Scale(Rectangle2D rect, float a, Vector2D anchor);
 Rectangle2D Rectangle2D_Rotate(Rectangle2D rect, float theta, Vector2D anchor);
+Rectangle2D Rectangle2D_RotateAll(Rectangle2D rect, float theta, Vector2D anchor)
 
 void Rectangle2D_Print(Rectangle2D rect);
 
